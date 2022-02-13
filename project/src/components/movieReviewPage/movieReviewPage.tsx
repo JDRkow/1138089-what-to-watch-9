33c0878973f235271699d8037c02ecbd1dca08fc
Олег Клявлин <oklyavlin@tourmalinecore.com>
2022-02-13 20:59:33 +0500

    lint: fix all lintigs errors

--- project/src/components/movieReviewPage/movieReviewPage.tsx
+++ project/src/components/movieReviewPage/movieReviewPage.tsx
@@ -83,7 +83,7 @@ export default function MovieReviewPage(): JSX.Element{
                 <div className="film-card__reviews-col">
                   <div className="review">
                     <blockquote className="review__quote">
-                      <p className="review__text">Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed films in years.</p>
+                      <p className="review__text">Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director&#39;s funniest and most exquisitely designed films in years.</p>
 
                       <footer className="review__details">
                         <cite className="review__author">Kate Muir</cite>
@@ -96,7 +96,7 @@ export default function MovieReviewPage(): JSX.Element{
 
                   <div className="review">
                     <blockquote className="review__quote">
-                      <p className="review__text">Anderson's films are too precious for some, but for those of us willing to lose ourselves in them, they're a delight. "The Grand Budapest Hotel" is no different, except that he has added a hint of gravitas to the mix, improving the recipe.</p>
+                      <p className="review__text">Anderson&#39;s films are too precious for some, but for those of us willing to lose ourselves in them, they&#39;re a delight. &#34;The Grand Budapest Hotel&#34; is no different, except that he has added a hint of gravitas to the mix, improving the recipe.</p>
 
                       <footer className="review__details">
                         <cite className="review__author">Bill Goodykoontz</cite>
@@ -109,7 +109,7 @@ export default function MovieReviewPage(): JSX.Element{
 
                   <div className="review">
                     <blockquote className="review__quote">
-                      <p className="review__text">I didn't find it amusing, and while I can appreciate the creativity, it's an hour and 40 minutes I wish I could take back.</p>
+                      <p className="review__text">I didn&#39;t find it amusing, and while I can appreciate the creativity, it&#39;s an hour and 40 minutes I wish I could take back.</p>
 
                       <footer className="review__details">
                         <cite className="review__author">Amanda Greever</cite>
